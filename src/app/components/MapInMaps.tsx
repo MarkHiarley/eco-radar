18ec3529efe8f6442f61aa1a4031b18a30d11316
MarkHiarley <hiarleycostamarley@gmail.com>
2025-03-31 21:06:51 -0300

    vendo como eu vou encaixar um mapa nessa div

---
+++ src/app/components/MapInMaps.tsx
@@ -0,0 +1,10 @@
+
+const MapInMaps = () => {
+    return (
+        <div className="flex flex-col justify-center items-center w-full h-full px-4">
+            <img src="https://pm1.aminoapps.com/8213/d1d6963a50a6647a7ede906863cbec1279b50ce6r1-300-168v2_00.jpg" alt="teste" className="w-5xl" />
+        </div>
+    )
+}
+
+export default MapInMaps;
