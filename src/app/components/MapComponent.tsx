57619c5b5cb931900bc2d05caa9ac5c29b7989d3
isaacsaless <isaacnascimentosales@gmail.com>
2025-04-01 19:41:02 -0300

    add - package

--- src/app/components/MapComponent.tsx
+++ src/app/components/MapComponent.tsx
@@ -8,23 +8,35 @@ import markerShadow from "leaflet/dist/images/marker-shadow.png";
 interface MarkerData {
   latitude: number;
   longitude: number;
+  frp: number;
 }
 
 export default function MapComponent({ markerList = [] }) {
-  if (typeof window !== "undefined") {
-    const customMarkerIcon = L.icon({
-      iconUrl: markerIcon.src,
-      iconRetinaUrl: markerIcon2x.src || markerIcon2x,
-      shadowUrl: markerShadow.src || markerShadow,
-      iconSize: [25, 41],
-      iconAnchor: [12, 41],
-      popupAnchor: [1, -34],
-      tooltipAnchor: [16, -28],
-      shadowSize: [41, 41],
-    });
+  // if (typeof window !== "undefined") {
+  //   const customMarkerIcon = L.icon({
+  //     iconUrl: markerIcon.src,
+  //     iconRetinaUrl: markerIcon2x.src || markerIcon2x,
+  //     shadowUrl: markerShadow.src || markerShadow,
+  //     iconSize: [25, 41],
+  //     iconAnchor: [12, 41],
+  //     popupAnchor: [1, -34],
+  //     tooltipAnchor: [16, -28],
+  //     shadowSize: [41, 41],
+  //   });
 
-    L.Marker.prototype.options.icon = customMarkerIcon;
-  }
+  //   L.Marker.prototype.options.icon = customMarkerIcon;
+  // }
+
+  const createColoredIcon = () => {
+    return L.divIcon({
+      html: `
+        <div class="marker bg-blue-500"></div>
+      `,
+      iconSize: [24, 24],
+      iconAnchor: [12, 12],
+      popupAnchor: [0, -12]
+    });
+  };
 
   return (
     <MapContainer
@@ -39,14 +51,14 @@ export default function MapComponent({ markerList = [] }) {
       />
       {markerList.length > 0 ? (
         markerList.map((marker: MarkerData, index) => (
-          <Marker key={index} position={[marker.latitude, marker.longitude]}>
+          <Marker key={index} position={[marker.latitude, marker.longitude] } icon={createColoredIcon()}>
             <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
           </Marker>
         ))
       ) : (
-        <Marker position={[-3.718, -38.543]}>
+        <Marker position={[-3.718, -38.543]} icon={createColoredIcon()}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
