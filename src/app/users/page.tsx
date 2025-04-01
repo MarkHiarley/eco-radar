159bc82132f07ce540e8a5f7cbcf861fd2177476
MarkHiarley <hiarleycostamarley@gmail.com>
2025-03-31 21:06:59 -0300

    novos componentes

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,7 +1,14 @@
-export default function Users(){
-    return(
-        <div className="h-screen w-screen bg-white ">
-            tela de user aqui
+import LateralBar from "../components/LateralBar";
+import MapInMaps from "../components/MapInMaps";
+export default function Users() {
+    return (
+        <div className="flex h-screen w-screen bg-white">
+            <div>
+                <LateralBar />
+            </div>
+            <div>
+                <MapInMaps />
+            </div>
         </div>
     )
 }
