b92e2022200e50c4960c2a390e2360bf42c54eec
isaacsaless <isaacnascimentosales@gmail.com>
2025-04-11 11:13:18 -0300

    fix - zoom

--- src/app/components/MapComponent.tsx
+++ src/app/components/MapComponent.tsx
@@ -13,7 +13,7 @@ interface MarkerData {
 }
 
 export default function MapComponent({ markerList = [] }) {
-  const customIconColor = (frp: number) => {
+  const customIcon = (frp: number) => {
     let PinImage;
     if (frp < 5) {
       PinImage = PinAmarelo;
@@ -29,35 +29,53 @@ export default function MapComponent({ markerList = [] }) {
       iconUrl: PinImage.src,
       iconSize: [48, 48],
       iconAnchor: [22, 38],
-      popupAnchor: [-3, -76],
+      popupAnchor: [2, -36],
     });
   };
+
+  const customPopUp = (frp: number) => {
+    let nivelQueimada;
+    if (frp < 5) {
+      nivelQueimada = "Baixo";
+    } else if (frp < 20 && frp >= 5) {
+      nivelQueimada = "Média";
+    } else if (frp < 50 && frp >= 20) {
+      nivelQueimada = "Grande ⚠️";
+    } else {
+      nivelQueimada = "Extrema ⚠️";
+    }
+
+    return <p><b>Essa queimada é considerada uma queimada de nível:</b> {nivelQueimada}</p>;
+  }
 
   return (
     <MapContainer
       center={[-3.718, -38.543]}
       zoom={13}
+      minZoom={4}
+      maxZoom={18}
       scrollWheelZoom={true}
       className="h-full w-full"
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        noWrap={true}
       />
       {markerList.length > 0 ? (
         markerList.map((marker: MarkerData, index) => (
           <Marker
             key={index}
             position={[marker.latitude, marker.longitude]}
-            icon={customIconColor(marker.frp)}
+            icon={customIcon(marker.frp)}
           >
             <Popup>
-              {marker.frp} <br /> Easily customizable.
+              {customPopUp(marker.frp)}
             </Popup>
           </Marker>
         ))
       ) : (
-        <Marker position={[-3.718, -38.543]} icon={customIconColor(1)}>
+        <Marker position={[-3.718, -38.543]} icon={customIcon(1)}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
