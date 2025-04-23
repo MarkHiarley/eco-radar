d3fdcf2ea65d442f2248c1d68cd529690802449d
isaacsaless <isaacnascimentosales@gmail.com>
2025-04-23 11:18:26 -0300

    add- google auth

---
+++ src/app/test/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+import { useSession, signIn } from "next-auth/react";
+
+export default function TestPage() {
+    const {data: session} = useSession();
+
+if(!session){
+    return(
+        <div>
+    <h1>Não logado</h1>
+    <button onClick={() => signIn("google")}>loge</button>
+    </div>
+)
+}
+
+    return (
+        <div>
+        <h1>Test Page</h1>
+        <p>This is a test page.</p>
+        </div>
+    );
+}
